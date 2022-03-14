61c5549a30bcab17785e2cf0de4cfde133ea2128
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-03-14 14:52:50 +0200

    Add missing override

--- src/main/java/ru/team/todo/validation/rules/task/MaxTaskNameLengthRule.java
+++ src/main/java/ru/team/todo/validation/rules/task/MaxTaskNameLengthRule.java
@@ -5,6 +5,8 @@ import ru.team.todo.validation.ValidationException;
 import ru.team.todo.validation.rules.ValidationRule;
 
 public class MaxTaskNameLengthRule<T extends NameableTaskRequest> implements ValidationRule<T> {
+
+    @Override
     public void onValidate(T task) {
         if (task.getName().length() > 15) {
             throw new ValidationException("Task name should not exceed 15 characters.");
