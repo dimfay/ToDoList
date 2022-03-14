75060faa33f0a0461c3b106331b00748b469383f
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-03-14 14:49:44 +0200

    Improve menu

--- src/main/java/ru/team/todo/ToDoApplication.java
+++ src/main/java/ru/team/todo/ToDoApplication.java
@@ -3,16 +3,6 @@ package ru.team.todo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import ru.team.todo.ui.commands.task.AddTaskCommand;
-import ru.team.todo.ui.commands.task.DeleteTaskByIdCommand;
-import ru.team.todo.ui.commands.task.DeleteTaskByNameCommand;
-import ru.team.todo.ui.commands.task.FindTasksCommand;
-import ru.team.todo.ui.commands.task.LinkTaskCommand;
-import ru.team.todo.ui.commands.task.UnlinkTaskCommand;
-import ru.team.todo.ui.commands.user.AddUserCommand;
-import ru.team.todo.ui.commands.user.DeleteUserCommand;
-import ru.team.todo.ui.commands.user.FindAllUsersCommand;
-import ru.team.todo.ui.commands.user.SwitchUserCommand;
 import ru.team.todo.ui.Menu;
 
 public class ToDoApplication {
@@ -21,17 +11,7 @@ public class ToDoApplication {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(ToDoConfiguration.class);
 
-        new Menu()
-                .addCommand(context.getBean(AddTaskCommand.class))
-                .addCommand(context.getBean(FindTasksCommand.class))
-                .addCommand(context.getBean(DeleteTaskByIdCommand.class))
-                .addCommand(context.getBean(DeleteTaskByNameCommand.class))
-                .addCommand(context.getBean(LinkTaskCommand.class))
-                .addCommand(context.getBean(UnlinkTaskCommand.class))
-                .addCommand(context.getBean(AddUserCommand.class))
-                .addCommand(context.getBean(FindAllUsersCommand.class))
-                .addCommand(context.getBean(SwitchUserCommand.class))
-                .addCommand(context.getBean(DeleteUserCommand.class))
-                .startUI();
+        var menu = context.getBean(Menu.class);
+        menu.startUI();
     }
 }
