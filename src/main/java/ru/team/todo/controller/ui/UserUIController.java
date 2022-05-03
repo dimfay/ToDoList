d84faafdb17dd28de85ae87d4fb5b95c2853a448
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-05-03 21:32:18 +0300

    Add ui controllers

---
+++ src/main/java/ru/team/todo/controller/ui/UserUIController.java
@@ -0,0 +1,36 @@
+package ru.team.todo.controller.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.team.todo.dto.users.AddUserRequest;
+import ru.team.todo.dto.users.FindUserRequest;
+import ru.team.todo.dto.users.FindUserResponse;
+import ru.team.todo.services.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ui")
+public class UserUIController {
+
+    private final UserService userService;
+
+    public UserUIController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAllUsers(Model model) {
+        FindUserResponse response = this.userService.findUsers(new FindUserRequest(List.of()));
+        model.addAttribute("users", response.getUsers());
+        return "users";
+    }
+    @GetMapping("/adduser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new AddUserRequest());
+        return "adduser";
+    }
+
+}
