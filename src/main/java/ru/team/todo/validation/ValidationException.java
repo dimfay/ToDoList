62aead951056aee37bad570ee46bff1d224c218c
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-05-27 13:51:10 +0300

    Remove unused old validation logic

--- src/main/java/ru/team/todo/validation/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.team.todo.validation;
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
